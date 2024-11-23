0539cfdae12cb0ad8e9cdfb8478f800d37a2b942
GaneshTidke <154491273+GOATGANESH@users.noreply.github.com>
2024-11-23 13:21:37 +0530

    Add files via upload
    Baics Done

---
+++ welcome.js
@@ -0,0 +1,8 @@
+console.log("Welcome back in js ");
+const userId=1234
+let userName="Ganesh"
+var userEmail="ganeshtidke45@gmail.com"
+let userPass="";
+
+console.table([userId,userName,userEmail,userPass])
+
